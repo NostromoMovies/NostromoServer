b772f23b4f10db09f83e59f9e8121396655b6912
stolan_b <sbelikove@gmail.com>
2024-11-09 22:25:45 -0800

    begin work on Quartz job scheduling system

---
+++ Nostromo.Server/Scheduling/HashFileJob.cs
@@ -0,0 +1,20 @@
+﻿using Nostromo.Server.Scheduling.Jobs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Nostromo.Server.Scheduling;
+public class HashFileJob : BaseJob
+{
+    public override string Name => throw new NotImplementedException();
+
+    public override string Type => throw new NotImplementedException();
+
+    public override Task ProcessJob()
+    {
+        throw new NotImplementedException();
+    }
+}
---
+++ Nostromo.Server/Scheduling/Jobs/BaseJob.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.Extensions.Logging;
+using Nostromo.Server.Scheduling.Jobs;
+using Quartz;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Nostromo.Server.Scheduling.Jobs;
+
+public abstract class BaseJob : IJob
+{ 
+    public ILogger _logger;
+    public abstract string Name { get; }
+    public abstract string Type { get; }
+    public async Task Execute(IJobExecutionContext context)
+    {
+        try
+        {
+            await ProcessJob();
+        }
+        catch (Exception ex)
+        {
+            //log exception or whatever
+        }
+    }
+
+    public abstract Task ProcessJob();
+}
+
+public abstract class BaseJob<T> : BaseJob
+{
+    public override abstract Task<T> ProcessJob();
+}
