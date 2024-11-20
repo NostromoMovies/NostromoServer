3ac5ec3f9e2b2c002b93d2f223aa1330748e17db
mooren85 <132626835+mooren85@users.noreply.github.com>
2024-11-19 21:49:23 -0800

    Update ServerSettings.cs
    Update ServerSettings

--- Nostromo.Server/Settings/ServerSettings.cs
+++ Nostromo.Server/Settings/ServerSettings.cs
@@ -1,9 +1,6 @@
 ﻿using System.ComponentModel.DataAnnotations;
-<<<<<<< HEAD
-=======
 using NHibernate.Linq.Functions;
 using NHibernate.Mapping;
->>>>>>> fea4bb2 (TmdbController/ServerSettings update)
 
 namespace Nostromo.Server.Settings
 {
@@ -22,10 +19,7 @@ namespace Nostromo.Server.Settings
 
         [Range(1, 65535, ErrorMessage = "Server Port must be between 1 and 65535")]
         public ushort ServerPort { get; set; } = 8112;
-<<<<<<< HEAD
-=======
         public string TmdbApiKey { get; set; } = "cbd64d95c4c66beed284bd12701769ec";
->>>>>>> fea4bb2 (TmdbController/ServerSettings update)
         public bool FirstRun { get; set; } = true;
     }
 }
