1d5c9b29db59e586444c78c0483d455694673847
stolan_b <sbelikove@gmail.com>
2024-11-25 23:41:58 -0800

    fix nullable GetByIdAsync

--- Nostromo.Server/Database/Repositories/IRepository.cs
+++ Nostromo.Server/Database/Repositories/IRepository.cs
@@ -1,7 +1,7 @@
 ﻿namespace Nostromo.Server.Database.Repositories;
 public interface IRepository<T> where T : class
 {
-    Task<T> GetByIdAsync(int id);
+    Task<T>? GetByIdAsync(int id);
     Task<IEnumerable<T>> GetAllAsync();
     Task<T> AddAsync(T entity);
     Task UpdateAsync(T entity);
--- Nostromo.Server/Database/Repositories/Repository.cs
+++ Nostromo.Server/Database/Repositories/Repository.cs
@@ -19,7 +19,7 @@ public class Repository<T> : IRepository<T> where T : class
         return _dbSet;
     }
 
-    public virtual async Task<T> GetByIdAsync(int id)
+    public virtual async Task<T>? GetByIdAsync(int id)
     {
         return await _dbSet.FindAsync(id);
     }
