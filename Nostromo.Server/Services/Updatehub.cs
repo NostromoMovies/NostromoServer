411b61cc93cfde8604809cd076ef131a0a84525c
Dominicpham03 <91446784+Dominicpham03@users.noreply.github.com>
2025-02-10 15:24:56 -0800

    Merged branch with main and updated Updatehub.cs

--- Nostromo.Server/Services/Updatehub.cs
+++ Nostromo.Server/Services/Updatehub.cs
@@ -1,6 +1,14 @@
-namespace Nostromo.Server.Services;
+using Microsoft.AspNetCore.SignalR;
+using System.Threading.Tasks;
 
-public class Updatehub
+
+namespace Nostromo.Server.Services
 {
-    
+    public class ProgressHub : Hub
+    {
+        public async Task SendProgressUpdate(string jobId, string message, int progress)
+        {
+            await Clients.All.SendAsync("ReceiveProgressUpdate", jobId, message, progress);
+        }
+    }
 }
