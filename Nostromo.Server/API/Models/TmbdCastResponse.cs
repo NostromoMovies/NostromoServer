808b63f967fcca1611c67753b3e3ea1700d45dbf
Dominicpham03 <91446784+Dominicpham03@users.noreply.github.com>
2025-02-11 18:14:45 -0800

    Started Cast Servies

---
+++ Nostromo.Server/API/Models/TmbdCastResponse.cs
@@ -0,0 +1,7 @@
+public class CastMember
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public string ProfilePath { get; set; }
+    public string Character { get; set; }
+}
