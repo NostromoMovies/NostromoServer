2463157dfb49eae8f01eb7df4fdd99459dd2959e
Stolan00 <sbelikove@gmail.com>
2025-03-15 15:55:39 -0700

    not working yet

---
+++ Nostromo.Server/API/Controllers/MediaStreamController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Quartz;
+using Nostromo.Server.Scheduling.Jobs;
+using Microsoft.AspNetCore.Http;
+using Nostromo.Server.API.Models;
+using Nostromo.Server.Services;
+using Nostromo.Server.Database.Repositories;
+
+namespace Nostromo.Server.API.Controllers
+{ 
+    [ApiController]
+    [Route("api/media")]
+    public class MediaStreamController : ControllerBase
+    {
+        private readonly MediaPlaybackService _mediaPlaybackService;
+        private readonly ILogger _logger;
+
+        public MediaStreamController(MediaPlaybackService mediaPlaybackService, ILogger logger)
+        {
+            _mediaPlaybackService = mediaPlaybackService;
+            _logger = logger;
+        }
+
+        [HttpGet("stream/{videoId}")]
+        public async Task<IResult> StreamVideo(int videoId)
+        {
+            var videoPath = await _mediaPlaybackService.GetVideoPath(videoId);
+
+            if (!System.IO.File.Exists(videoPath))
+                return Results.NotFound("Video not found");
+
+            var stream = new FileStream(videoPath, FileMode.Open, FileAccess.Read, FileShare.Read);
+            return Results.File(stream, "video/x-matroska", enableRangeProcessing: true);
+        }
+    }
+}
---
+++ Nostromo.Server/Services/MediaPlaybackService.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.Extensions.Logging;
+using Nostromo.Server.Database.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Nostromo.Server.Services
+{
+    public class MediaPlaybackService
+    {
+        private readonly IVideoPlaceRepository _videoPlaceRepository;
+        private readonly ILogger _logger;
+
+        public MediaPlaybackService(IVideoPlaceRepository videoPlaceRepository, ILogger logger)
+        {
+            _videoPlaceRepository = videoPlaceRepository;
+            _logger = logger;
+        }
+
+        public async Task<string> GetVideoPath(int videoId)
+        {
+            return await _videoPlaceRepository.GetVideoFilePathByVideoID(videoId);
+        }
+    }
+}
